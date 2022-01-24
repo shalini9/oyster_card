a4bab0f7eacf7df355a7fa4a9c4d9907db1bfb34
shalini9 <shalini.nangia1@gmail.com>
2022-01-24 16:59:01 +0000

    exercise 6 part way complete

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,10 +1,19 @@
 class Oystercard
   
+  MAXIMUM_BALANCE = 90
+
   attr_reader :balance
   
   def initialize
     @balance = 0
   end 
-  
+
+  def top_up(money)
+    raise "You have exceed the maximum balance #{MAXIMUM_BALANCE}" if money + @balance > MAXIMUM_BALANCE
+    @balance += money
+  end 
+
+
+
 
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,17 @@ describe Oystercard do
     expect(oystercard.balance).to eq(0)
   end 
 
+  it "can top-up balance" do
+    oystercard = Oystercard.new
+    oystercard.balance
+    expect(oystercard.top_up(5)).to eq(5)
+  end 
+
+  it "can issue error if balance is over maximum limit" do
+    oystercard = Oystercard.new
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    oystercard.top_up maximum_balance
+    expect(oystercard.top_up(4)).to raise_error("You have exceed the maximum balance #{maximum_balance}")
+  end 
 
 end 
