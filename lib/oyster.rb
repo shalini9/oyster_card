bb673d7cdd9730f1e5f0695fd3d3de485b8a67aa
shalini9 <shalini.nangia1@gmail.com>
2022-01-25 15:32:10 +0000

    finished exercise 10

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,11 +1,13 @@
 class Oystercard
   
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
 
   attr_reader :balance
   
   def initialize(balance = 0)
     @balance = balance
+    @in_journey = false
   end 
 
   def top_up(money)
@@ -13,9 +15,25 @@ class Oystercard
     fail "#{@balance} cannot exceed #{MAXIMUM_BALANCE}" if (money + @balance) > MAXIMUM_BALANCE
     @balance += money
   end 
+ 
+  def in_journey?
+    @in_journey
+  end 
 
+  def touch_in
+    @in_journey = true
+    fail "You do not have minimum balance of £1 to travel" if @balance < MINIMUM_BALANCE
+  end
+  
+  def touch_out
+    deduct(MINIMUM_BALANCE)
+    @in_journey = false
+  end
 
-
-
+  private
+  def deduct(money)
+    @balance -= money
+  end
+  
 end 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,24 +2,51 @@ require 'oyster'
 
 describe Oystercard do
   it "can check the starting balance is 0" do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq(0)
+    expect(subject.balance).to eq(0)
   end 
 
   it "can top-up balance" do
-    oystercard = Oystercard.new
-    oystercard.balance
-    expect(oystercard.top_up(5)).to eq(5)
+    subject.balance
+    expect(subject.top_up(5)).to eq(5)
   end 
 
   it "can issue error if balance is over maximum limit" do
-    oystercard = Oystercard.new
     maximum_balance = Oystercard::MAXIMUM_BALANCE
-    oystercard.top_up(90)
-    expect { oystercard.top_up (1) }.to raise_error "#{oystercard.balance} cannot exceed #{maximum_balance}"
+    subject.top_up(90)
+    expect { subject.top_up (1) }.to raise_error "#{subject.balance} cannot exceed #{maximum_balance}"
   end 
 
+  it "deduct from balance" do
+    subject.top_up(11)
+    expect { subject.deduct(10) }.to change{subject.balance}.by -10
+  end
+  
+  it "is initially not in a journey" do
+    expect(subject).not_to be_in_journey
+    # in_journey? use 'be_' and remove '?'' and for boolean methods
+  end
 
+  it "can touch in" do
+    subject.top_up(5)
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
 
+  it "can touch out" do
+    subject.top_up(5)
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+  
+  it "can issue error if balance is under minimum balance £1" do
+    expect { subject.touch_in }.to raise_error "You do not have minimum balance of £1 to travel"
+  end 
+
+  it "can deduct fare from balance when you touch out" do
+    subject.top_up(5)
+    subject.touch_in        
+    expect { subject.touch_out }.to change{subject.balance}.by (-Oystercard::MINIMUM_BALANCE)
+  end 
 
 end 
