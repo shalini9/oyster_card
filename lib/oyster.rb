38fb6d79d3ad2a087f928291eda645744b36bed0
Oliver Cutting <oliver.cutting@gmail.com>
2022-01-25 17:11:40 +0000

    Step12 complete

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -3,11 +3,11 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station, :exit_station, :journeys
   
   def initialize(balance = 0)
     @balance = balance
-    @in_journey = false
+    @journeys = {}
   end 
 
   def top_up(money)
@@ -17,17 +17,20 @@ class Oystercard
   end 
  
   def in_journey?
-    @in_journey
+    !!entry_station
   end 
 
-  def touch_in
-    @in_journey = true
+  def touch_in(entry_station)
     fail "You do not have minimum balance of £1 to travel" if @balance < MINIMUM_BALANCE
+    @entry_station = entry_station
   end
-  
-  def touch_out
+
+  def touch_out(exit_station)
     deduct(MINIMUM_BALANCE)
-    @in_journey = false
+    @exit_station = exit_station
+    @journeys[:entry_station] = entry_station
+    @journeys[:exit_station] = exit_station
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,10 @@
 require 'oyster'
 
 describe Oystercard do
+  let(:entry_station){ double :station }
+  let(:exit_station){ double :station }
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station}}
+
   it "can check the starting balance is 0" do
     expect(subject.balance).to eq(0)
   end 
@@ -9,44 +13,63 @@ describe Oystercard do
     subject.balance
     expect(subject.top_up(5)).to eq(5)
   end 
-
-  it "can issue error if balance is over maximum limit" do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    subject.top_up(90)
-    expect { subject.top_up (1) }.to raise_error "#{subject.balance} cannot exceed #{maximum_balance}"
-  end 
-
-  it "deduct from balance" do
-    subject.top_up(11)
-    expect { subject.deduct(10) }.to change{subject.balance}.by -10
-  end
   
   it "is initially not in a journey" do
     expect(subject).not_to be_in_journey
     # in_journey? use 'be_' and remove '?'' and for boolean methods
   end
 
-  it "can touch in" do
-    subject.top_up(5)
-    subject.touch_in
-    expect(subject).to be_in_journey
-  end
-
-  it "can touch out" do
-    subject.top_up(5)
-    subject.touch_in
-    subject.touch_out
-    expect(subject).not_to be_in_journey
-  end
-  
   it "can issue error if balance is under minimum balance £1" do
-    expect { subject.touch_in }.to raise_error "You do not have minimum balance of £1 to travel"
+    expect { subject.touch_in(entry_station) }.to raise_error "You do not have minimum balance of £1 to travel"
   end 
 
-  it "can deduct fare from balance when you touch out" do
-    subject.top_up(5)
-    subject.touch_in        
-    expect { subject.touch_out }.to change{subject.balance}.by (-Oystercard::MINIMUM_BALANCE)
-  end 
+  it 'has and empty list of journeys by default' do
+    expect(subject.journeys).to be_empty
+  end
 
+  context 'is already in_journey' do
+    before do
+      subject.instance_variable_set(:@entry_station, entry_station)
+    end
+    
+    it 'stores exit station' do
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+
+    it "can touch out" do
+      subject.touch_out(exit_station)
+      expect(subject).not_to be_in_journey
+    end
+
+    it 'stores the entry station' do
+      expect(subject.entry_station).to eq entry_station
+    end
+
+    it "can deduct fare from balance when you touch out" do      
+      expect { subject.touch_out(exit_station) }.to change{subject.balance}.by (-Oystercard::MINIMUM_BALANCE)
+      end 
+  end
+
+  context 'card has balance' do
+    before do
+      subject.instance_variable_set(:@balance, 90)
+    end
+
+    it "can issue error if balance is over maximum limit" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      expect { subject.top_up (1) }.to raise_error "#{subject.balance} cannot exceed #{maximum_balance}"
+    end 
+
+    it "can touch in" do
+      subject.touch_in(entry_station)
+      expect(subject).to be_in_journey
+    end
+
+    it 'stores a journey' do
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.journeys).to include journey
+    end
+  end
 end 
